ee5ae4372ee93bb1753f7fd8daca6bc25f8db619
Sosnus <sosnus15@gmail.com>
2024-06-26 08:09:47 +0200

    add more hw platforms

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker build --tag sosnus15/pinger .
-docker push sosnus15/pinger
+docker build --platform linux/amd64,linux/arm64 --tag sosnus15/pinger:1.0.3 .
+# docker build --platform arm64 --tag sosnus15/pinger:1.02-arm .
+docker push sosnus15/pinger:1.0.3
