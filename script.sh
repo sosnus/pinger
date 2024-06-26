c507e306e14f5dff7ed6a0102d8b37429ecc9460
Sosnus <sosnus15@gmail.com>
2024-06-26 03:46:22 +0200

    add small delay on startup

--- script.sh
+++ script.sh
@@ -3,6 +3,8 @@ echo "PARAMETERS:"
 echo "VNAME=$VNAME"
 echo "VURL=$VURL"
 echo "VTIME=$VTIME"
+sleep 3
+echo "START!"
 while true; do
     echo -n "[$(date +'%Y-%m-%d %H:%M:%S')]  "
     echo -n "send ping {$VNAME} response: "
