b0b502398dd426803504e5b5b5e95ccf9bbeaca0
Sosnus <sosnus15@gmail.com>
2025-07-05 01:57:06 +0200

    change platform

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build --platform linux/amd64,linux/arm64 --tag sosnus15/pinger:1.0.3 .
+docker buildx build --platform=linux/amd64,linux/arm64 --tag sosnus15/pinger:1.0.04 .
+docker push sosnus15/pinger:1.0.04
 # docker build --platform arm64 --tag sosnus15/pinger:1.02-arm .
-docker push sosnus15/pinger:1.0.3
