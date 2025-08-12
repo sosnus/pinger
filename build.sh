92fe31272c262d71254e8175f031947c513f8353
Sosnus <sosnus15@gmail.com>
2025-08-13 01:42:16 +0200

    rm sosnus15

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker buildx build --platform=linux/amd64,linux/arm64 --tag sosnus15/pinger:1.0.04 .
-docker push sosnus15/pinger:1.0.04
+docker buildx build --platform=linux/amd64,linux/arm64 --tag sosnus/pinger:1.0.05 .
+docker push sosnus/pinger:1.0.05
 # docker build --platform arm64 --tag sosnus15/pinger:1.02-arm .
